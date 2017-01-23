95743a3268c86efb18616f9224bffcba8d1a51ee
David Kluszczynski <dave07747@gmail.com>
2017-01-23 08:50:28 -0500

    tcp

--- app/src/main/java/com/lrnplex/whodat/MainActivity.java
+++ app/src/main/java/com/lrnplex/whodat/MainActivity.java
@@ -1,15 +1,11 @@
 package com.lrnplex.whodat;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
-import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -24,16 +20,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
-import java.util.ArrayList;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,11 +35,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView2;
     private ImageView imageView;
     private Bitmap bp;
-    private Uri fileUri;
-    String picturePath;
-    Uri selectedImage;
-    String ba1;
-    public static String URL = "10.251.76.43:7539";
+    TcpClient mTcpClient;
+    String text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,6 +63,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onDestroy() {
+        //Close
+        if (mTcpClient != null) {
+            mTcpClient.stopClient();
+        }
+        super.onDestroy();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -108,80 +106,63 @@ public class MainActivity extends AppCompatActivity {
         if(resultCode == RESULT_OK) {
             bp = (Bitmap) data.getExtras().get("data");
             capturedImage.setImageBitmap(bp);
-            upload();
-        }
 
-        if (requestCode == 100 && resultCode == RESULT_OK) {
+            new ConnectTask().execute("");
+            //sends the message to the server
+            Thread thread = new Thread() {
+                {
+                    String string = BitMapToString(bp);
+                    Log.d("Length", "onActivityResult: " + string.length());
 
-            selectedImage = data.getData();
+                    mTcpClient.sendMessage(BitMapToString(bp));
+                    Log.d("Message Sent: ", BitMapToString(bp));
 
-            // Cursor to get image uri to display
-
-            String[] filePathColumn = {MediaStore.Images.Media.DATA};
-            Cursor cursor = getContentResolver().query(selectedImage,
-                    filePathColumn, null, null, null);
-            cursor.moveToFirst();
-
-            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            picturePath = cursor.getString(columnIndex);
-            cursor.close();
-
-
+                }
+            };
+            thread.start();
         }
     }
 
-    private void upload() {
-        // Image location URL
-        Log.e("path", "----------------" + picturePath);
+    public String BitMapToString(Bitmap bitmap){
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteFormat = stream.toByteArray();
+        String imgString = Base64.encodeToString(byteFormat, Base64.NO_WRAP);
+        if(imgString.length() % 4 != 0){
+            imgString += '=' * (4-imgString.length() % 4);
+        }
 
-        // Image
-        ByteArrayOutputStream bao = new ByteArrayOutputStream();
-        bp.compress(Bitmap.CompressFormat.JPEG, 90, bao);
-        byte[] ba = bao.toByteArray();
-        //ba1 = Base64.encodeBytes(ba);
-
-        Log.e("base64", "-----" + ba1);
-
-        // Upload image to server
-        new uploadToServer().execute();
-
+        Log.d("Length", "BitMapToString: " + imgString.length());
+        return imgString;
     }
 
-    public class uploadToServer extends AsyncTask<Void, Void, String> {
 
-        private ProgressDialog pd = new ProgressDialog(MainActivity.this);
-        protected void onPreExecute() {
-            super.onPreExecute();
-            pd.setMessage("Wait image uploading!");
-            pd.show();
+    public class ConnectTask extends AsyncTask<String, String, TcpClient> {
+
+        @Override
+        protected TcpClient doInBackground(String... message) {
+
+            //we create a TCPClient object
+            mTcpClient = new TcpClient(new TcpClient.OnMessageReceived() {
+                @Override
+                //here the messageReceived method is implemented
+                public void messageReceived(String message) {
+                    //this method calls the onProgressUpdate
+                    publishProgress(message);
+                }
+            });
+            mTcpClient.run();
+
+            return null;
         }
 
         @Override
-        protected String doInBackground(Void... params) {
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+            //response received from server
+            Log.d("test", "response " + values[0]);
+            //process server response here....
 
-            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-            nameValuePairs.add(new BasicNameValuePair("base64", ba1));
-            nameValuePairs.add(new BasicNameValuePair("ImageName", System.currentTimeMillis() + ".jpg"));
-            try {
-                HttpClient httpclient = new DefaultHttpClient();
-                HttpPost httppost = new HttpPost(URL);
-                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-                HttpResponse response = httpclient.execute(httppost);
-                String st = EntityUtils.toString(response.getEntity());
-                Log.v("log_tag", "In the try Loop" + st);
-
-            } catch (Exception e) {
-                Log.v("log_tag", "Error in http connection " + e.toString());
-            }
-            return "Success";
-
-        }
-
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            pd.hide();
-            pd.dismiss();
         }
     }
 }
-
